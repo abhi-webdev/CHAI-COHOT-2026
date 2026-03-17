bf86c3822360699f66c685b92c9b1c9b72cb107d
Abhimanyu kumar <166692284+abhi-webdev@users.noreply.github.com>
2026-03-17 08:54:14 +0530

    completed project model

--- BACKEND/03_Project_Camp_Backend/src/routes/auth.route.js
+++ BACKEND/03_Project_Camp_Backend/src/routes/auth.route.js
@@ -1,13 +1,48 @@
-import {Router} from "express"
-import { login, logout, registerUser } from "../controllers/auth.controller.js"
-import { validate } from "../middlewares/validator.middleware.js"
-import {userLoginValidate, userRegistervalidaor} from "../validators/index.js"
-import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { Router } from "express";
+import {
+  changedPassword,
+  forgotPasswordRequest,
+  getCurrentUser,
+  login,
+  logout,
+  refreshAccessToken,
+  registerUser,
+  resendEmailVerification,
+  resetForgotPassword,
+  verifyEmail,
+} from "../controllers/auth.controller.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import {
+  userChangedCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userLoginValidate,
+  userRegistervalidaor,
+  userResetForgotPasswordValidator,
+} from "../validators/index.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router = Router()
+const router = Router();
 
-router.route("/register").post(userRegistervalidaor(), validate,  registerUser)
-router.route("/login").post(userLoginValidate() , login)
-router.route("/logout").post(verifyJWT, logout)
+// unsecured routes
+router.route("/register").post(userRegistervalidaor(), validate, registerUser);
+router.route("/login").post(userLoginValidate(), validate, login);
+router.route("/verifyEmail/:verificationToken").get(verifyEmail);
+router.route("/refresh-token").post(refreshAccessToken);
+router
+  .route("/forgot-password")
+  .post(userForgotPasswordValidator(), validate, forgotPasswordRequest);
+router
+  .route("/reset-password/:resetToken")
+  .post(userResetForgotPasswordValidator(), validate, resetForgotPassword);
 
-export default router
+// secure routes
+router.route("/logout").post(verifyJWT, logout);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router
+  .route("/change-password")
+  .post(userChangedCurrentPasswordValidator(), validate, changedPassword);
+router
+  .route("/resend-email-verification")
+  .post(verifyJWT, resendEmailVerification);
+
+export default router;
