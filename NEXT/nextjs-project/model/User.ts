b519c0e9897a62363b96139cb5b13d25c4085dee
Abhimanyu kumar <166692284+abhi-webdev@users.noreply.github.com>
2026-03-26 00:12:49 +0530

    next js user model complete

---
+++ NEXT/nextjs-project/model/User.ts
@@ -0,0 +1,71 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+export interface Message extends Document{
+    content : string;
+    createdAt : Date;
+}
+
+const MessageSchema : Schema<Message> = new Schema({
+    content : {
+        type : String,
+        required : true,
+    },
+    createdAt : { 
+        type : Date,
+        required : true,
+        default : Date.now
+    }
+})
+
+export interface User extends Document{
+    username : string;
+    email : string;
+    password : string;
+    verifyCode : string;
+    VerifyCodeExpiry : Date;
+    isVerified: boolean;
+    isAcceptingMessage : boolean;
+    messages : Message[]
+
+}
+
+const UserSchema : Schema<User> = new Schema({
+    username : {
+        type : String,
+        required : [true, "Username is required"],
+        trim : true,
+        unique : true
+    },
+    email : { 
+        type : String,
+        required : [true, "Email is required"],
+        trim : true,
+        unique : true,
+        match : [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Please use a valid email address']
+    },
+    password : { 
+        type : String,
+        required : [true, "Password is required"],
+    },
+    verifyCode : {
+        type : String,
+        required : [true, "Verify code is required"]
+    },
+    
+    VerifyCodeExpiry : {
+        type : Date,
+        required : [true, "VerifyExpiry expiry code is required"]
+    },
+    isVerified : {
+        type : Boolean,
+        default : false
+    },
+    isAcceptingMessage : {
+        type : Boolean,
+        default : true
+    },
+    messages : {
+        type : [MessageSchema]
+    }
+
+})
