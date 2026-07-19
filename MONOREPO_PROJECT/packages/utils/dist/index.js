d196f372a0b2c8d570fc0068383b1b93313972d3
Abhimanyu kumar <166692284+abhi-webdev@users.noreply.github.com>
2026-07-19 10:05:47 +0530

    chore: update pnpm workspace configuration to enable esbuild and ensure package paths are correctly defined

---
+++ MONOREPO_PROJECT/packages/utils/dist/index.js
@@ -0,0 +1,7 @@
+import { z } from "zod";
+export const createUserSchema = z.object({
+    name: z.string().min(3, "Name Must be atleat 3 charcter"),
+    email: z.email("Email is required"),
+    password: z.string().min(6, "Password must be atleast 6 character")
+});
+//# sourceMappingURL=index.js.map
